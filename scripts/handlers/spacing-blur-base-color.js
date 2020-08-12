08a6ab32189b8889c96a47a93c210e707bce290b
Olga <okozmovskaya@icloud.com>
2020-08-12 16:06:11 +0200

    Add space-blur-color effects

--- scripts/handlers/spacing-blur-base-color.js
+++ scripts/handlers/spacing-blur-base-color.js
@@ -1,14 +1,27 @@
 'use strict';
+'use strict';
 /* Add changes in html and css files*/ 
 
 /* Get Elements (with querySelector)*/
 
+window.onload = function () {
+    const inputs = document.getElementById("controls").querySelectorAll("input");
+    /* Write the function*/ 
+    inputs.forEach(addEventListenerToElem);
+}
 
+function addEventListenerToElem(elem) {
+    if (elem.type == 'range') {
+        elem.addEventListener('mousemove', handleUpdate);
+    } else if (elem.type == 'color') {
+        elem.addEventListener('change', handleUpdate);
+    }
+}
 
-
-/* Write the function*/ 
-
-
-
+function handleUpdate() {
+    // debugger;
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  }
 
 /* write your event listeners in event-listeners.js*/
